b6049ed93249519dc3f136d7ee0c056cbffd6005
maomao <maomao.li@jjcargo.com>
2018-01-09 19:50:03 +0800

    add spring rest docs maven project

---
+++ spring-rest-docs-maven/src/main/java/com/example/springrestdocsmaven/HomeController.java
@@ -0,0 +1,20 @@
+package com.example.springrestdocsmaven;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.Map;
+
+/**
+ * Created by maomao on 18/1/9.
+ */
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public Map<String, Object> greeting() {
+        return Collections.singletonMap("message", "Hello World");
+    }
+
+}
---
+++ spring-rest-docs-maven/src/main/java/com/example/springrestdocsmaven/SpringRestDocsMavenApplication.java
@@ -0,0 +1,12 @@
+package com.example.springrestdocsmaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestDocsMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestDocsMavenApplication.class, args);
+	}
+}
